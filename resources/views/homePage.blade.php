a1dc7521c3e795b2119578add038f994f082d329
ricardopmartins <ricardopmartins.gt.pvh@gmail.com>
2025-12-02 11:57:58 -0400

    Adicionando uma imagem de background na Home e estilizando com gradiente no styleHome.css

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -8,9 +8,47 @@
 
 @section('content')
 <body>
-    <section class="banner"> </section>
+    <section class="banner">
+        <div class="banner-overlay"></div>
+        <img src="{{ asset('assets/images/bannerHome.png') }}" alt="Banner promocional" class="banner-img">
+    </section>
+
     <h1>HOME</h1>
     <p> In eget pharetra elit, non placerat leo. Ut malesuada lectus at augue commodo lobortis. Phasellus tempus, lorem porttitor tincidunt euismod, arcu neque aliquet enim, nec consectetur tellus nulla quis sem.</p>
+
+    </section>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+    <h1>gnoiaengpanwg</h1>
+
 </body>
 
 @endsection
