432d08b56927c2a98e7ed6079ff4a1d9dac7f97a
ricardopmartins <ricardopmartins.gt.pvh@gmail.com>
2025-12-15 11:45:07 -0400

    Adicionando um carrossel a página Home e incluindo uma variavel promocoes para exibir apenas jogos com descontos maiores ou iguais 50% no carrossel

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -16,19 +16,19 @@
     <h1>HOME</h1>
     <p> In eget pharetra elit, non placerat leo. Ut malesuada lectus at augue commodo lobortis. Phasellus tempus, lorem porttitor tincidunt euismod, arcu neque aliquet enim, nec consectetur tellus nulla quis sem.</p>
 
-    {{-- <section class="container my-5">
+    <section class="container my-5">
         <div id="promoCarousel" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
             @foreach($promocoes as $index => $jogo)
               <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
-                <img src="{{ $jogo->imagem }}" class="d-block w-100" style="height: 400px; object-fit: cover;" alt="{{ $jogo->nome_jogo }}">
-                <div class="carousel-caption d-none d-md-block bg-dark bg-opacity-50 rounded p-3">
-                  <h5>{{ $jogo->nome_jogo }}</h5>
+                <img src="{{ $jogo->imagem }}" class="d-block w-100" style="height: 800px; object-fit: cover;" alt="{{ $jogo->nome_jogo }}">
+                <div class="carousel-caption d-none d-md-block bg-white bg-opacity-35 rounded p-3">
+                  <h5 class="text-black">{{ $jogo->nome_jogo }}</h5>
                   <p>
                     <span class="text-decoration-line-through text-muted">R$ {{ number_format($jogo->valor, 2, ',', '.') }}</span>
-                    <span class="fw-bold text-warning ms-2">R$ {{ number_format($jogo->final_price, 2, ',', '.') }}</span>
+                    <span class="fw-bold text-success ms-2">R$ {{ number_format($jogo->final_price, 2, ',', '.') }}</span>
                   </p>
-                  <a href="{{ route('jogos.show', $jogo->id_jogo) }}" class="btn btn-primary">Ver promoção</a>
+                  <a href="{{ route('homePage') }}" class="btn btn-primary">Ver promoção</a>
                 </div>
               </div>
             @endforeach
@@ -41,7 +41,7 @@
             <span class="carousel-control-next-icon"></span>
           </button>
         </div>
-    </section> --}}
+    </section>
 
 
     <section class="container my-5">
